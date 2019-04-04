3ccf16d1d276eb287b5a876c04d0c6e9c5e34db0
Alex <adamm@urbn.com>
2019-04-04 18:49:43 -0400

    build folder

---
+++ build/precache-manifest.207d80c555c98cdb053842c3fa354268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0688a82856ae92cd079",
+    "url": "/static/css/main.b8197272.chunk.css"
+  },
+  {
+    "revision": "a0688a82856ae92cd079",
+    "url": "/static/js/main.221803ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "eb00f4bddb387f5b2704de3bdad76e56",
+    "url": "/index.html"
+  }
+];
