ee85dc5eacc915cf12e4f14d09f8d86ea7c670d1
Alex <adamm@urbn.com>
2019-04-04 21:18:57 -0400

    onclick functionality

--- src/App.js
+++ src/App.js
@@ -9,46 +9,74 @@ class App extends Component {
     cards: [
       {
         "id": 1,
-        "name": "SpongeBob",
-        "image": "https://vignette.wikia.nocookie.net/spongebobgalaxy/images/0/07/SpongeBob_SquarePants.png/revision/latest?cb=20171228024014",
+        "name": "hammer",
+        "image": "https://www.woodblocx.co.uk/media/catalog/product/cache/1/thumbnail/960x/05e17a266b0e9cc26fb81a2e0bed7e78/l/u/lump-hammer.jpg",
       },
       {
         "id": 2,
-        "name": "Mr. Krabs",
-        "image": "https://vignette3.wikia.nocookie.net/vsbattles/images/8/80/Mr._Krabs.png/revision/latest?cb=20150919162131",
+        "name": "hammer",
+        "image": "https://images-na.ssl-images-amazon.com/images/I/61fnMMgkGIL._SX425_.jpg",
       },
       {
         "id": 3,
-        "name": "Squidward",
-        "image": "https://vignette2.wikia.nocookie.net/fictionalcharacters/images/a/ac/Squidward.png/revision/latest?cb=20131121012626",
+        "name": "hammer",
+        "image": "https://i5.walmartimages.com/asr/5ec60309-22a2-454f-a632-35d7d0d55ffc_1.2a918275def7979179f7f3b85a4f1209.jpeg",
       },
       {
         "id": 4,
-        "name": "Squidward",
-        "image": "https://vignette2.wikia.nocookie.net/fictionalcharacters/images/a/ac/Squidward.png/revision/latest?cb=20131121012626",
+        "name": "hammer",
+        "image": "https://d2kx81irxb72bi.cloudfront.net/cs/products/38/73/08/550x550.jpg",
       },
       {
         "id": 5,
-        "name": "Squidward",
-        "image": "https://vignette2.wikia.nocookie.net/fictionalcharacters/images/a/ac/Squidward.png/revision/latest?cb=20131121012626",
+        "name": "hammer",
+        "image": "https://ae01.alicdn.com/kf/HTB1FjN5KFXXXXa6XFXXq6xXFXXXx/1-1-Scale-Full-Metal-Thor-Hammer-Mjolnir-1-1-Replica-Thor-Custom-Cosplay-Hammer-Collection.jpg_640x640.jpg",
       },
       {
         "id": 6,
-        "name": "Squidward",
-        "image": "https://vignette2.wikia.nocookie.net/fictionalcharacters/images/a/ac/Squidward.png/revision/latest?cb=20131121012626",
+        "name": "hammer",
+        "image": "https://www.harborfreight.com/media/catalog/product/i/m/image_20800.jpg",
+      },
+      {
+        "id": 7,
+        "name": "hammer",
+        "image": "https://www.wind-lock.com/product/image/medium/w-go05164_1.jpg",
+      },
+      {
+        "id": 8,
+        "name": "hammer",
+        "image": "http://www2.fiskars.com/var/fiskars_amer/storage/images/frontpage2/products/home-improvement-tools/hammers/isocore-22-oz-milled-face-framing-hammer-16/942701-20-eng-US/IsoCore-22-oz-Milled-face-Framing-Hammer-16.jpg",
+      },
+      {
+        "id": 9,
+        "name": "hammer",
+        "image": "https://cdn.shopify.com/s/files/1/0248/6216/products/hardcore-hammer-7.jpg?v=1537308705",
       }
     ]
   };
+
+  shuffle = a => {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  handleClick = () => {
+    alert("you clicked");
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
-        {this.state.cards.map(card => (
+        {this.shuffle(this.state.cards).map(card => (
           <GameCard
             id={card.id}
             image={card.image}
             name={card.image}
+            handleClick={this.handleClick}
           />
         ))}
       </div>
