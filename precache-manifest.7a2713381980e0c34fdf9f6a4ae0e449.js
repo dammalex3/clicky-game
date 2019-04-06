9c5d537a0c36c40450cd17570e7f3fd19943e2d8
Alex <adamm@urbn.com>
2019-04-06 09:27:14 -0400

    Updates

---
+++ precache-manifest.7a2713381980e0c34fdf9f6a4ae0e449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72bc0a8e846b603f01af",
+    "url": "/clickygame/static/css/main.abd87210.chunk.css"
+  },
+  {
+    "revision": "72bc0a8e846b603f01af",
+    "url": "/clickygame/static/js/main.340136f9.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clickygame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "03a4871dc9b7cc6f6c16fea2c544afc1",
+    "url": "/clickygame/index.html"
+  }
+];
